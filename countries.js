65d566c5332d20caf4ddb4c377d1fb7c0f43c64c
Gilberto Hernandez <gilbertoh56050@students.laalliance.org>
2017-11-17 17:39:58 +0000

    Done.

---
+++ countries.js
@@ -0,0 +1,6 @@
+var countries = [
+    "Chad", "Cuba", "Greenland", "Iraq", "Mali", "Oman", "India"
+    ]
+    countries.splice(8, 0, "fiji")
+    countries.splice(3, 0, "Iran")
+    console.log(countries)
